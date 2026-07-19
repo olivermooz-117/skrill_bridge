599df82ec296271ed4b0cf40ef831c599e917c1c
olivermooz-117 <olivermooz@gmail.com>
2026-07-19 16:43:34 +0300

    fix

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -2,3 +2,4 @@ from app.routes.auth import auth_bp
 from app.routes.gigs import gigs_bp
 from app.routes.orders import orders_bp
 from app.routes.users import users_bp
+from app.utils.decorators import role_required
